2e0a865c66c083cc81dd99df62477fb5bf13d5be
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-01-25 12:09:44 +0530

    [tejasri]Add. Implementation of Algorithm programs

---
+++ AlgorithmPrograms/PrimePalindrome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class PrimePalindrome
+    {
+        public static void isPrime()
+        {
+
+        }
+
+
+    }
+}
---
+++ AlgorithmPrograms/SquareRoot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class SquareRoot
+    {
+        public static void findSquareRoot()
+        {
+            Console.Write("enter a number to find square root:");
+            double c = Utility.readDouble();
+            double epsilon = 1.0e-15;
+            double t = c;
+            while(Math.Abs(t-c/t)>epsilon*t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+            Console.WriteLine("square root of"+c+"is : "+t);
+        }
+    }
+}
