fa876797eefb611c5c51a161d5bec9f7dbd41ec4
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-01-28 13:12:13 +0530

    [tejasri]refactor. updated programs

---
+++ AlgorithmPrograms/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class InsertionSort
+    {
+        public static void Sort()
+        {
+            Console.Write("enter number of strings :");
+            int n = Utility.readInt();
+            String[] array=new String[n];
+            String temp;
+            int j;
+            Console.WriteLine("enter elements of an array");
+            for(int i=0;i<array.Length;i++)
+            {
+                array[i] = Utility.readString().ToLower();
+            }
+            for(int i=1;i<array.Length;i++)
+            {
+                temp = array[i];
+                j = i;
+                while ((j >0) && (array[j-1].CompareTo(temp) > 0))
+                {
+                    
+                    array[j] = array[j-1];
+                    j=j-1;
+                }
+                array[j] = temp;
+            }
+            Console.WriteLine("After sorting the strings are :");
+            for(int i=0;i<array.Length;i++)
+            {
+                Console.WriteLine(array[i] +" ");
+            }
+        }
+    }
+}
---
+++ AlgorithmPrograms/MagicNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class MagicNumber
+    {
+		public static int Number(int low, int high)
+		{
+			int str1 = 1;
+			int str2 = 2;
+			if ((high - low) == 0)
+			{
+				return high;
+			}
+			int mid = (low + high) / 2;
+			Console.WriteLine("type 1 if it is less than  " + mid);
+			Console.WriteLine("type 2 if it is greater than " + mid);
+			int str = Utility.readInt();
+			if (str == str1)
+			{
+				high = mid;
+				return Number(low, high);
+			}
+			else if (str == str2)
+			{
+				low = mid;
+				return Number(low, high);
+			}
+			else if (high == mid)
+			{
+				return mid;
+			}
+			else
+			{
+				Console.WriteLine(" invalid input");
+			}
+
+			return low;
+		}
+		public static void GuessNumber()
+		{
+			int n = 8;
+			int N = (int)Math.Pow(2, n);
+			Console.WriteLine(" think of a number between 0 and " + (N - 1));
+			Console.WriteLine("your number is" + Number(0, N));
+		}
+
+	}
+}
+
+    
