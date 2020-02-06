aace015c0f077293e399f8ec5ae4047cac0ee73a
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-06 19:22:43 +0530

    [tejasri]Add . Implementation of Inventory Data Management

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.InventoryDetails");
+            Console.WriteLine("enter an option to get executed");
+            int option = Utility.readInt();
+            switch(option)
+            {
+                case 1:
+                    InventoryDetails.ReadingFromFile();
+                    break;
+            }
+        }
+    }
+}
