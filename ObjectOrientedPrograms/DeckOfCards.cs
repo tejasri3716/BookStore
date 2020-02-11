13629ad7876700853115e5f63bbff7b5eec7c201
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-11 11:13:30 +0530

    [tejasri]Added Implementation of method for distributing cards in deck of cards

--- ObjectOrientedPrograms/DeckOfCards.cs
+++ ObjectOrientedPrograms/DeckOfCards.cs
@@ -8,7 +8,13 @@ namespace ObjectOrientedPrograms
     {
         public void DistributeCards()
         {
-         
+            String[] Suits = { "Club", "Diamond", "Heart", "Spade" };
+            String[] Rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+            String[,] Cards = new String[4, 13];
+            String[,] Players = new String[4, 13];
+            Utility.CardArray(Suits, Cards, Rank);
+
+
         }
     }
 }
