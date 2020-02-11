374bd864722bb72c30dc67d1316174a654396a43
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-11 17:30:09 +0530

    [tejasri]Add . Method for Shuffling of cards in deck of cards

---
+++ ObjectOrientedPrograms/StockManagement/StockPortfolio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedPrograms
+{
+    public class StockPortfolio
+    {
+        public List<StockInfo> TotalStocks{get;set;}
+    }
+}
