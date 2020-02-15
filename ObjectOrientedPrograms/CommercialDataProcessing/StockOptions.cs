d69b1343ae341ea9c53e0af4902e7728cc016dd1
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-15 12:32:57 +0530

    [tejasri]Added . class for selecting an option in Commercial Data Processing

---
+++ ObjectOrientedPrograms/CommercialDataProcessing/StockOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedPrograms.CommercialDataProcessing
+{
+    /// <summary>
+    /// Class to select any of the Options Listed Below
+    /// </summary>
+    class StockOptions
+    {
+        Stocks stocks = null;
+    }
+}
