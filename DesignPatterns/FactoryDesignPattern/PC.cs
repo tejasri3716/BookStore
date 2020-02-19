46cc0fce8549c8fcef1df9a0098a0550a60c841a
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-19 19:20:15 +0530

    [tejasri]Refactor . Modified PC class in Factory Design Pattern

--- DesignPatterns/FactoryDesignPattern/PC.cs
+++ DesignPatterns/FactoryDesignPattern/PC.cs
@@ -1,34 +1,53 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="PrototypeTest.cs" company="Bridgelabz">
+// <copyright file="PC.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.FactoryDesignPattern
 {
+    using System;
+    /// <summary>
+    /// PC class implements Computer class
+    /// </summary>
+    /// <seealso cref="DesignPatterns.FactoryDesignPattern.Computer" />
     class PC : Computer
     {
         private string ram;
         private string hdd;
         private string cpu;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PC"/> class.
+        /// </summary>
+        /// <param name="ram">The ram.</param>
+        /// <param name="hdd">The HDD.</param>
+        /// <param name="cpu">The cpu.</param>
         public PC(String ram, String hdd, String cpu)
         {
             this.ram = ram;
             this.hdd = hdd;
             this.cpu = cpu;
         }
+        /// <summary>
+        /// Gets the ram.
+        /// </summary>
+        /// <returns></returns>
         public string getRAM()
         {
             return this.ram;
         }
+        /// <summary>
+        /// Gets the HDD.
+        /// </summary>
+        /// <returns></returns>
         public string getHDD()
         {
             return this.hdd;
         }
+        /// <summary>
+        /// Gets the cpu.
+        /// </summary>
+        /// <returns></returns>
         public string getCPU()
         {
             return this.cpu;
