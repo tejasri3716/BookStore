24311eced6cecb4377ba471e8ca186aa69341dd9
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-19 19:26:05 +0530

    [tejasri]Refactor . Modified EmployeeAbstractClass in Prototype Design Pattern

--- DesignPatterns/PrototypeDesignPattern/EmployeeAbstractClass.cs
+++ DesignPatterns/PrototypeDesignPattern/EmployeeAbstractClass.cs
@@ -1,19 +1,25 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="PrototypeTest.cs" company="Bridgelabz">
+// <copyright file="EmployeeAbstractClass.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.PrototypeDesignPattern
 {
-   public abstract class EmployeeAbstractClass
+    /// <summary>
+    /// EmployeeAbstractClass consists of abstract methods
+    /// </summary>
+    public abstract class EmployeeAbstractClass
     {
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns></returns>
         public abstract Employee Clone();
-
+        /// <summary>
+        /// Gets the details.
+        /// </summary>
+        /// <returns></returns>
         public abstract string GetDetails();
     }
 }
