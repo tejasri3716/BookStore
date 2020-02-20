fda0822a8fcf3fc4216191e4429ebf85226688ec
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 12:44:53 +0530

    [tejasri]Add . Implementation of Socket Adapter interface in Adapter Design Pattern

---
+++ DesignPatterns/AdapterDesignPattern/SocketAdapterInterface.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns.AdapterDesignPattern
+{
+    /// <summary>
+    /// Interface for socket and adapter
+    /// </summary>
+    interface SocketAdapterInterface
+    {
+
+        /// <summary>
+        /// Get 120 volts.
+        /// </summary>
+        /// <returns></returns>
+        public Volt Get120Volts();
+        /// <summary>
+        /// Get 12 volts.
+        /// </summary>
+        /// <returns></returns>
+        public Volt Get12Volts();
+        /// <summary>
+        /// Get 3 volts.
+        /// </summary>
+        /// <returns></returns>
+        public Volt Get3Volts();
+    }
+}
