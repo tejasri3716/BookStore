b2f49b3e224d8889240e57710e0095b9e92f8d4e
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 19:05:45 +0530

    [tejasri]Refactor . Modified  Socket Adapter Interface in Adapter Design Pattern Programs

--- DesignPatterns/AdapterDesignPattern/ISocketAdapterInterface.cs
+++ DesignPatterns/AdapterDesignPattern/ISocketAdapterInterface.cs
@@ -1,5 +1,5 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="ComputerFactory.cs" company="Bridgelabz">
+// <copyright file="ISocketAdapterInterface.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
@@ -9,23 +9,24 @@ namespace DesignPatterns.AdapterDesignPattern
     /// <summary>
     /// Interface for socket and adapter
     /// </summary>
-    interface SocketAdapterInterface
+    public interface ISocketAdapterInterface
     {
-
         /// <summary>
         /// Get 120 volts.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>method for getting 120 volts</returns>
         public Volt Get120Volts();
+
         /// <summary>
         /// Get 12 volts.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>method for getting 12 volts</returns>
         public Volt Get12Volts();
+
         /// <summary>
         /// Get 3 volts.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>method for getting 3 volts</returns>
         public Volt Get3Volts();
     }
 }
