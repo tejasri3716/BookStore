7bc1b4f031c775060c139a6f26063183aebeb1bb
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 19:18:16 +0530

    [tejasri]Refactor . Modified  FacadeDesignPattern class in Design Pattern Programs

--- DesignPatterns/FacadeDesignPattern/FacadeDesignPattern.cs
+++ DesignPatterns/FacadeDesignPattern/FacadeDesignPattern.cs
@@ -1,36 +1,63 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="ComputerFactory.cs" company="Bridgelabz">
+// <copyright file="FacadeDesignPattern.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-
 namespace DesignPatterns.FacadeDesignPattern
 {
-    class FacadeDesignPattern
+    /// <summary>
+    /// FacadeDesignPattern class uses IShopping interface
+    /// </summary>
+    public class FacadeDesignPattern
     {
-
+        /// <summary>
+        /// The headphones
+        /// </summary>
         private IShopping headphones;
+
+        /// <summary>
+        /// The mobile
+        /// </summary>
         private IShopping mobile;
+
+        /// <summary>
+        /// The laptop
+        /// </summary>
         private IShopping laptop;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FacadeDesignPattern"/> class.
+        /// </summary>
         public FacadeDesignPattern()
         {
-            headphones = new HeadPhones();
-            mobile = new Mobile();
-            laptop = new Laptop();
+            this.headphones = new HeadPhones();
+            this.mobile = new Mobile();
+            this.laptop = new Laptop();
         }
+
+        /// <summary>
+        /// Buys the head phones.
+        /// </summary>
         public void BuyHeadPhones()
         {
-            headphones.BuyItem();
+            this.headphones.BuyItem();
         }
+
+        /// <summary>
+        /// Buys the mobile.
+        /// </summary>
         public void BuyMobile()
         {
-            mobile.BuyItem();
+            this.mobile.BuyItem();
         }
+
+        /// <summary>
+        /// Buys the laptop.
+        /// </summary>
         public void BuyLaptop()
         {
-            laptop.BuyItem();
+            this.laptop.BuyItem();
         }
     }
 }
-
