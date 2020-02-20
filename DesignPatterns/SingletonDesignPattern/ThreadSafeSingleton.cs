5864030a2ec4a7598bcc141f959eca1d5e5ec53e
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 10:07:43 +0530

    [tejasri]Refactor . Modified Thread Safe Singleton class in singleton Design Pattern

--- DesignPatterns/SingletonDesignPattern/ThreadSafeSingleton.cs
+++ DesignPatterns/SingletonDesignPattern/ThreadSafeSingleton.cs
@@ -1,25 +1,34 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="PrototypeTest.cs" company="Bridgelabz">
+// <copyright file="ThreadSafeSingleton.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.SingletonDesignPattern
 {
+    using System;
+    /// <summary>
+    /// ThreadSafeSingleton class is used to intialize object 
+    /// </summary>
     class ThreadSafeSingleton
     {
         private static ThreadSafeSingleton instance = null;
         private static int counter = 0;
         private static readonly object obj = new object();
+        /// <summary>
+        /// Prevents a default instance of the <see cref="ThreadSafeSingleton"/> class from being created.
+        /// </summary>
         private ThreadSafeSingleton()
         {
             counter++;
             Console.WriteLine("counter value is :" + counter);
         }
+        /// <summary>
+        /// Gets the object if it is null
+        /// </summary>
+        /// <value>
+        /// The get instance.
+        /// </value>
         public static ThreadSafeSingleton GetInstance
         {
             get
@@ -35,6 +44,10 @@ namespace DesignPatterns.SingletonDesignPattern
                 return instance;
             }
         }
+        /// <summary>
+        /// Prints the details.
+        /// </summary>
+        /// <param name="message">The message.</param>
         public void PrintDetails(String message)
         {
             Console.WriteLine(message);
