a377dedf688ca3a7ce9090bf6b6414585deed814
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 18:58:20 +0530

    [tejasri]Refactor . Modified  Socket Adapter Implementation class in Adapter Design Pattern Programs

--- DesignPatterns/AdapterDesignPattern/SocketAdapterImplementation.cs
+++ DesignPatterns/AdapterDesignPattern/SocketAdapterImplementation.cs
@@ -1,34 +1,54 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="ComputerFactory.cs" company="Bridgelabz">
+// <copyright file="SocketAdapterImplementation.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.AdapterDesignPattern
 {
+    /// <summary>
+    /// SocketAdapterImplementation class implements SocketAdapterInterface interface
+    /// </summary>
+    /// <seealso cref="DesignPatterns.AdapterDesignPattern.SocketAdapterInterface" />
     class SocketAdapterImplementation : SocketAdapterInterface
     {
         Socket socket = new Socket();
+
+        /// <summary>
+        /// Get 120 volts.
+        /// </summary>
+        /// <returns>gets 120 volts</returns>
         public Volt Get120Volts()
         {
           return socket.GetVolt();
         }
 
+        /// <summary>
+        /// Get 12 volts.
+        /// </summary>
+        /// <returns>gets 12 volts</returns>
         public Volt Get12Volts()
         {
             Volt volt = socket.GetVolt();
             return ConvertVolts(volt, 10);
         }
 
+        /// <summary>
+        /// Get 3 volts.
+        /// </summary>
+        /// <returns>gets 3 volts</returns>
         public Volt Get3Volts()
         {
             Volt volt = socket.GetVolt();
             return ConvertVolts(volt, 40);
         }
+
+        /// <summary>
+        /// Converts the volts.
+        /// </summary>
+        /// <param name="volt">The volt.</param>
+        /// <param name="i">The i.</param>
+        /// <returns></returns>
         public Volt ConvertVolts(Volt volt, int i)
         {
             return new Volt(volt.GetVolts() / i);
