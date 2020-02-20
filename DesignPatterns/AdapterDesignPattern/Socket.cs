22df76b38f08f70c1af1021c2f85d859485799e0
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 18:55:05 +0530

    [tejasri]Refactor . Modified  Socket class in Adapter Design Pattern Programs

--- DesignPatterns/AdapterDesignPattern/Socket.cs
+++ DesignPatterns/AdapterDesignPattern/Socket.cs
@@ -1,41 +1,26 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="ComputerFactory.cs" company="Bridgelabz">
+// <copyright file="Socket.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.AdapterDesignPattern
 {
-      public class Socket
+    /// <summary>
+    /// Socket class containing a method to get volt
+    /// </summary>
+    public class Socket
         {
-            public Volt volts;
+        /// <summary>
+        /// Creates object volts for Volt class
+        /// </summary>
+        public Volt volts;
 
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Socket"/> class.
-            /// </summary>
-           /* public Socket()
-            {
-                volts = new Volt();
-            }
-*/
-            /// <summary>
-            /// Initializes a new instance of the <see cref="Socket"/> class.
-            /// </summary>
-            /// <param name="volt">The volt.</param>
-            /*public Socket(int volt)
-            {
-                volts = new Volt(volt);
-            }
-*/
-            /// <summary>
-            /// returs the volts
-            /// </summary>
-            /// <returns></returns>
-            public Volt GetVolt()
+        /// <summary>
+        /// Gets the volt.
+        /// </summary>
+        /// <returns></returns>
+        public Volt GetVolt()
             {
             return new Volt(); ;
             }
