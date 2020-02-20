1a98a3e625ed14337ca64f95e729a05492edadf5
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 19:23:05 +0530

    [tejasri]Refactor . Modified  HeadPhones class in Design Pattern Programs

--- DesignPatterns/FacadeDesignPattern/HeadPhones.cs
+++ DesignPatterns/FacadeDesignPattern/HeadPhones.cs
@@ -1,17 +1,22 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="ComputerFactory.cs" company="Bridgelabz">
+// <copyright file="HeadPhones.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.FacadeDesignPattern
 {
-    class HeadPhones : IShopping
+    using System;
+
+    /// <summary>
+    /// HeadPhones class implements IShopping Interface
+    /// </summary>
+    /// <seealso cref="DesignPatterns.FacadeDesignPattern.IShopping" />
+    public class HeadPhones : IShopping
     {
+        /// <summary>
+        /// Buys the item.
+        /// </summary>
         public void BuyItem()
         {
             Console.WriteLine("your order of head phones is out for delivery");
