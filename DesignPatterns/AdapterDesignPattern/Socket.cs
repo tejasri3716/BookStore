88ee80275fb9069baa253b1ae2288a087c3e2dc1
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 12:45:51 +0530

    [tejasri]Add . Implementation of Socket class in Adapter Design Pattern

---
+++ DesignPatterns/AdapterDesignPattern/Socket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.AdapterDesignPattern
+{
+      public class Socket
+        {
+            public Volt volts;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Socket"/> class.
+            /// </summary>
+            public Socket()
+            {
+                volts = new Volt();
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Socket"/> class.
+            /// </summary>
+            /// <param name="volt">The volt.</param>
+            public Socket(int volt)
+            {
+                volts = new Volt(volt);
+            }
+
+            /// <summary>
+            /// returs the volts
+            /// </summary>
+            /// <returns></returns>
+            public Volt GetSocket()
+            {
+                return this.volts;
+            }
+        }
+}
