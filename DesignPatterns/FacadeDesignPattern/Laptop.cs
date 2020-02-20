c8663e7116877b20502ba597599b4096d4e40c0b
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 19:27:20 +0530

    [tejasri]Refactor . Modified Laptop class in Design Pattern Programs

--- DesignPatterns/FacadeDesignPattern/Laptop.cs
+++ DesignPatterns/FacadeDesignPattern/Laptop.cs
@@ -1,17 +1,22 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="ComputerFactory.cs" company="Bridgelabz">
+// <copyright file="Laptop.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.FacadeDesignPattern
 {
-    class Laptop : IShopping
+    using System;
+
+    /// <summary>
+    /// Laptop class implements IShopping interface
+    /// </summary>
+    /// <seealso cref="DesignPatterns.FacadeDesignPattern.IShopping" />
+    public class Laptop : IShopping
     {
+        /// <summary>
+        /// Buys the item.
+        /// </summary>
         public void BuyItem()
         {
             Console.WriteLine("your order for laptop is out for delivery");
