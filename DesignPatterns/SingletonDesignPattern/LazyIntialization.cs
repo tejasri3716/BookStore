c967b120dddba24ddcbe318e9b4c91cf0ea20d25
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 09:59:09 +0530

    [tejasri]Refactor . Modified Lazy Intialization class in singleton Design Pattern

--- DesignPatterns/SingletonDesignPattern/LazyIntialization.cs
+++ DesignPatterns/SingletonDesignPattern/LazyIntialization.cs
@@ -1,24 +1,33 @@
 ﻿//--------------------------------------------------------------------------------------------------------------------
-// <copyright file="PrototypeTest.cs" company="Bridgelabz">
+// <copyright file="LazyIntialization.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace DesignPatterns.SingletonDesignPattern
 {
+    using System;
+    /// <summary>
+    /// Sealed Class Lazy Intialization used check the no of objects intialized
+    /// </summary>
     public sealed class LazyIntialization
     {
         private static LazyIntialization instance = null;
         private static int counter = 0;
+        /// <summary>
+        /// Prevents a default instance of the <see cref="LazyIntialization"/> class from being created.
+        /// </summary>
         private LazyIntialization()
         {
             counter++;
             Console.WriteLine("counter value is :" + counter);
         }
+        /// <summary>
+        /// Gets the object if it is null
+        /// </summary>
+        /// <value>
+        /// The get instance.
+        /// </value>
         public static LazyIntialization GetInstance
         {
             get
@@ -30,6 +39,10 @@ namespace DesignPatterns.SingletonDesignPattern
                 return instance;
             }
         }
+        /// <summary>
+        /// Prints the details.
+        /// </summary>
+        /// <param name="message">The message.</param>
         public void PrintDetails(String message)
         {
             Console.WriteLine(message);
