81b21489f76b5f68a43275bc3e661bbbf927c61a
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-20 12:43:44 +0530

    [tejasri]Add . Implementation of Volt class in Adapter Design Pattern

---
+++ DesignPatterns/AdapterDesignPattern/Volt.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns.AdapterDesignPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+	public class Volt
+	{
+
+		private int volts;
+		public Volt()
+		{
+			this.volts = 240;
+		}
+
+		public Volt(int v)
+		{
+			this.volts = v;
+		}
+
+		public int getVolts()
+		{
+			return volts;
+		}
+
+		public void setVolts(int volts)
+		{
+			this.volts = volts;
+		}
+
+	}
+}
+
+
