f94c6da4095e3928cd9c2af6282559aa0eeffaa2
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-21 12:52:11 +0530

    [tejasri]Refactor . Modified CommandExecutorProxyPattern class in Proxy Design Pattern

--- DesignPatterns/StructuralDesignPattern/ProxyDesignPattern/CommandExecutorProxyPattern.cs
+++ DesignPatterns/StructuralDesignPattern/ProxyDesignPattern/CommandExecutorProxyPattern.cs
@@ -1,24 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="CommandExecutorProxyPattern.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace DesignPatterns.StructuralDesignPattern.ProxyDesignPattern
 {
+    using System;
+
+    /// <summary>
+    /// CommandExecutorProxyPattern class is the Real class in Proxy Design Pattern
+    /// </summary>
+    /// <seealso cref="DesignPatterns.StructuralDesignPattern.ICommandExecutor" />
     public class CommandExecutorProxyPattern : ICommandExecutor
     {
+        /// <summary>
+        /// The admin
+        /// </summary>
         private bool isAdmin;
+
+        /// <summary>
+        /// The executor
+        /// </summary>
         private CommandExecutor executor;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandExecutorProxyPattern"/> class.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="pwd">The password.</param>
         public CommandExecutorProxyPattern(string user, string pwd)
         {
-            if ("tejasri".Equals(user) && "tejasri@3716".Equals(pwd)) isAdmin = true;
-            executor = new CommandExecutor();
+            if ("tejasri".Equals(user) && "tejasri@3716".Equals(pwd))
+            {
+                this.isAdmin = true;
+            }
+
+            this.executor = new CommandExecutor();
         }
+
+        /// <summary>
+        /// Runs the command.
+        /// </summary>
+        /// <param name="cmd">The command.</param>
+        /// <exception cref="Exception">rm command is not allowed for non-admin users.</exception>
         public void RunCommand(string cmd)
         {
-            if (isAdmin)
+            if (this.isAdmin)
             {
-                executor.RunCommand(cmd);
+                this.executor.RunCommand(cmd);
             }
             else
             {
@@ -28,7 +58,7 @@ namespace DesignPatterns.StructuralDesignPattern.ProxyDesignPattern
                 }
                 else
                 {
-                    executor.RunCommand(cmd);
+                    this.executor.RunCommand(cmd);
                 }
             }
         }
