871e0f3c09abc28cbd03c187e32801478f79cd93
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-21 16:32:27 +0530

    [tejasri]Added . Implementation of ISubject interface in Observer Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/ObserverDesignPattern/ISubject.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.BehaviouralDesignPattern.ObserverDesignPattern
+{
+    public interface ISubject
+    {
+        /// <summary>
+        /// Registers the specified object.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public void Register(IObserver obj);
+
+        /// <summary>
+        /// UnRegister the observers.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        public void UnRegister(IObserver obj);
+
+        /// <summary>
+        /// Notifies the observers about changes.
+        /// </summary>
+        public void NotifyObservers();
+
+        /// <summary>
+        /// Gets the update.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public object GetUpdate(IObserver obj);
+    }
+}
