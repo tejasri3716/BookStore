bbd8340356141908d13e531f1a90aa121ec18a43
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-21 16:31:35 +0530

    [tejasri]Added . Implementation of IObserver interface in Observer Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/ObserverDesignPattern/IObserver.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.BehaviouralDesignPattern.ObserverDesignPattern
+{
+    public interface IObserver
+    {
+        /// <summary>
+        /// Updates the observer, used by subject 
+        /// </summary>
+        public void Update();
+
+        /// <summary>
+        /// Sets the subject.
+        /// </summary>
+        /// <param name="sub">The sub.</param>
+        public void SetSubject(ISubject sub);
+    }
+}
