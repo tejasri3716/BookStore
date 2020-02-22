4db500362489e8f29b59978859542db391da69c0
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 12:04:48 +0530

    [tejasri]Refactor . Modified KeyBoard class in Visitor Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/KeyBoard.cs
+++ DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/KeyBoard.cs
@@ -6,8 +6,16 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern
 {
+    /// <summary>
+    /// Keyboard class implements IComputerPart interface  and overrides Accept method
+    /// </summary>
+    /// <seealso cref="DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern.IComputerPart" />
     public class Keyboard : IComputerPart
     {
+        /// <summary>
+        /// Accepts the specified computer part visitor.
+        /// </summary>
+        /// <param name="computerPartVisitor">The computer part visitor.</param>
         public void Accept(IComputerPartVisitor computerPartVisitor)
         {
             computerPartVisitor.Visit(this);
