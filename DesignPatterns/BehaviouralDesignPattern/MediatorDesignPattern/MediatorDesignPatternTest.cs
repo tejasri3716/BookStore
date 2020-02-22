8168c51274a4765ba4b2c5aca6dab32eb0be1a3a
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 16:43:22 +0530

    [tejasri]Add . Implementation of MediatorDesignPatternTest class in Mediator Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/MediatorDesignPatternTest.cs
@@ -0,0 +1,22 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="MediatorDesignPatternTest.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
+{
+    public class MediatorDesignPatternTest
+    {
+        public static void Test()
+        {
+            IATCMediator aTCMediator= new ATCMediator();
+            Flight airAsia = new Flight(aTCMediator);
+            Runway runway = new Runway(aTCMediator);
+            aTCMediator.RegisterFlight(airAsia);
+            airAsia.GetReady();
+            runway.Land();
+            airAsia.Land();
+        }
+    }
+}
