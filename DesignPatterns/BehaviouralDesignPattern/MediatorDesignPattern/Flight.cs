bea2edd8a09ff5e17d037e525bb77eec5e2c9de7
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 16:40:17 +0530

    [tejasri]Add . Implementation of Flight class in Mediator Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/Flight.cs
@@ -0,0 +1,35 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="Flight.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
+{
+    using System;
+
+    public class Flight : ICommand
+    {
+        private IATCMediator aTCMediator;
+        public Flight(IATCMediator aTCMediator)
+        {
+            this.aTCMediator = aTCMediator;
+        }
+        public void Land()
+        {
+            if(aTCMediator.IsLandingOk())
+            {
+                Console.WriteLine("Successfully landed");
+                aTCMediator.SetLandingStatus(true);
+            }
+            else
+            {
+                Console.WriteLine("waiting for landing");
+            }
+        }
+        public void GetReady()
+        {
+            Console.WriteLine("Ready for landing");
+        }
+    }
+}
