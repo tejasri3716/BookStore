ef7e23939d21a90ab6a05b82ad9f559db29fab58
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 12:00:02 +0530

    [tejasri]Refactor . Modified IComputerPart interface in Visitor Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/IComputerPart.cs
+++ DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/IComputerPart.cs
@@ -6,8 +6,15 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern
 {
+    /// <summary>
+    /// IComputerPart interface consists of abstract method Accept
+    /// </summary>
     public interface IComputerPart
     {
+        /// <summary>
+        /// Accepts the specified computer part visitor.
+        /// </summary>
+        /// <param name="computerPartVisitor">The computer part visitor.</param>
         public void Accept(IComputerPartVisitor computerPartVisitor);
     }
 }
