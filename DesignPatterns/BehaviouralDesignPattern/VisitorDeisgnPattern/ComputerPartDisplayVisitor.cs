f2e468df54da50cdc3b64950e4360aeb979bb922
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 11:58:07 +0530

    [tejasri]Refactor . Modified ComputerPartDisplayVisitor class in Visitor Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/ComputerPartDisplayVisitor.cs
+++ DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/ComputerPartDisplayVisitor.cs
@@ -7,20 +7,44 @@
 namespace DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern
 {
     using System;
+
+    /// <summary>
+    /// ComputerPartDisplayVisitor implements IComputerPartVisitor interface consists of Visit method
+    /// </summary>
+    /// <seealso cref="DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern.IComputerPartVisitor" />
     public class ComputerPartDisplayVisitor : IComputerPartVisitor
     {
+        /// <summary>
+        /// Visits the specified computer.
+        /// </summary>
+        /// <param name="computer">The computer.</param>
         public void Visit(Computer computer)
         {
           Console.WriteLine("Displaying Computer.");
         }
+
+        /// <summary>
+        /// Visits the specified mouse.
+        /// </summary>
+        /// <param name="mouse">The mouse.</param>
         public void Visit(Mouse mouse)
         {
           Console.WriteLine("Displaying Mouse.");
         }
+
+        /// <summary>
+        /// Visits the specified keyboard.
+        /// </summary>
+        /// <param name="keyboard">The keyboard.</param>
         public void Visit(Keyboard keyboard)
         {
           Console.WriteLine("Displaying Keyboard.");
         }
+
+        /// <summary>
+        /// Visits the specified monitor.
+        /// </summary>
+        /// <param name="monitor">The monitor.</param>
         public void Visit(Monitor monitor)
         {
           Console.WriteLine("Displaying Monitor.");
