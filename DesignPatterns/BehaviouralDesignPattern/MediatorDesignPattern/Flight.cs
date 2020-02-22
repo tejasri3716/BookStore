f070a88360915eaca57b32f99e18ec4ac5ab4d1b
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 17:02:00 +0530

    [tejasri]Refactor . Modifed of Flight class in Mediator Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/Flight.cs
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/Flight.cs
@@ -8,25 +8,45 @@ namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
 {
     using System;
 
+    /// <summary>
+    /// Flight class implements ICommand interface , used to send request for landing
+    /// </summary>
+    /// <seealso cref="DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern.ICommand" />
     public class Flight : ICommand
     {
-        private IATCMediator aTCMediator;
-        public Flight(IATCMediator aTCMediator)
+        /// <summary>
+        /// Air Traffic Control mediator
+        /// </summary>
+        private IATCMediator atcMediator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Flight"/> class.
+        /// </summary>
+        /// <param name="atcMediator">Air Traffic Control mediator.</param>
+        public Flight(IATCMediator atcMediator)
         {
-            this.aTCMediator = aTCMediator;
+            this.atcMediator = atcMediator;
         }
+
+        /// <summary>
+        /// Land method sends the status of landing 
+        /// </summary>
         public void Land()
         {
-            if(aTCMediator.IsLandingOk())
+            if (this.atcMediator.IsLandingOk())
             {
                 Console.WriteLine("Successfully landed");
-                aTCMediator.SetLandingStatus(true);
+                this.atcMediator.SetLandingStatus(true);
             }
             else
             {
                 Console.WriteLine("waiting for landing");
             }
         }
+
+        /// <summary>
+        /// Gets ready for landing
+        /// </summary>
         public void GetReady()
         {
             Console.WriteLine("Ready for landing");
