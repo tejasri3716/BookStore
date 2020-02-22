45f78c6b41362368e5b8c05e38e9004ff7e0f20e
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 16:41:17 +0530

    [tejasri]Add . Implementation of ICommand interface in Mediator Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/ICommand.cs
@@ -0,0 +1,13 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="ICommand.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
+{
+    public interface ICommand
+    {
+        public void Land();
+    }
+}
