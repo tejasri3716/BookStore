d9f74a56da7ae90ab61154fcbc5267cdd8f1e099
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 17:05:35 +0530

    [tejasri]Refactor . Modified IATCMediator interface in Mediator Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/IATCMediator.cs
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/IATCMediator.cs
@@ -6,11 +6,38 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
 {
+    /// <summary>
+    /// IATCMediator interface consists of abstract methods to set landing status
+    /// to register the runway for landing 
+    /// to register the flight
+    /// to check whether runway is free for landing or not
+    /// </summary>
     public interface IATCMediator
     {
+        /// <summary>
+        /// Registers the runway.
+        /// </summary>
+        /// <param name="runway">The runway.</param>
         public void RegisterRunway(Runway runway);
+
+        /// <summary>
+        /// Registers the flight.
+        /// </summary>
+        /// <param name="flight">The flight.</param>
         public void RegisterFlight(Flight flight);
+
+        /// <summary>
+        /// Determines whether [is landing ok].
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if [is landing ok]; otherwise, <c>false</c>.
+        /// </returns>
         public bool IsLandingOk();
+
+        /// <summary>
+        /// Sets the landing status.
+        /// </summary>
+        /// <param name="status">if set to <c>true</c> [status].</param>
         public void SetLandingStatus(bool status);
     }
 }
