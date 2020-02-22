197aac7a1ac4082817c85d0a9c1785fd39fad81f
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 16:37:21 +0530

    [tejasri]Add . Implementation of IATCMediator interface in Mediator Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/IATCMediator.cs
@@ -0,0 +1,16 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="IATCMediator.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
+{
+    public interface IATCMediator
+    {
+        public void RegisterRunway(Runway runway);
+        public void RegisterFlight(Flight flight);
+        public bool IsLandingOk();
+        public void SetLandingStatus(bool status);
+    }
+}
