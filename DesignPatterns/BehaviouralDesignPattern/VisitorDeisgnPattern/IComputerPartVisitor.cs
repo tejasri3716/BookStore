451949a0dff1a941a5bb2d80b7a191746fc510ef
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 12:02:25 +0530

    [tejasri]Refactor . Modified IComputerPartVisitor interface in Visitor Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/IComputerPartVisitor.cs
+++ DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/IComputerPartVisitor.cs
@@ -6,12 +6,33 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern
 {
-	public interface IComputerPartVisitor
-	{
-		public void Visit(Computer computer);
-		public void Visit(Mouse mouse);
-		public void Visit(Keyboard keyboard);
-		public void Visit(Monitor monitor);
-	}
+    /// <summary>
+    /// IComputerPartVisitor interface consists of abstract method Visit to visit every object
+    /// </summary>
+    public interface IComputerPartVisitor
+    {
+        /// <summary>
+        /// Visits the specified computer.
+        /// </summary>
+        /// <param name="computer">The computer.</param>
+        public void Visit(Computer computer);
 
+        /// <summary>
+        /// Visits the specified mouse.
+        /// </summary>
+        /// <param name="mouse">The mouse.</param>
+        public void Visit(Mouse mouse);
+
+        /// <summary>
+        /// Visits the specified keyboard.
+        /// </summary>
+        /// <param name="keyboard">The keyboard.</param>
+        public void Visit(Keyboard keyboard);
+
+        /// <summary>
+        /// Visits the specified monitor.
+        /// </summary>
+        /// <param name="monitor">The monitor.</param>
+        public void Visit(Monitor monitor);
+    }
 }
