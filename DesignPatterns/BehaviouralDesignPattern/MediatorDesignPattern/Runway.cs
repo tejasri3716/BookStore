5bfb69c629a0bbf0122b4de646211388ead05749
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 16:42:12 +0530

    [tejasri]Add . Implementation of Runway class in Mediator Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/Runway.cs
@@ -0,0 +1,25 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="Runway.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
+{
+    using System;
+
+    public class Runway : ICommand
+    {
+        private IATCMediator aTCMediator;
+        public Runway(IATCMediator aTCMediator)
+        {
+            this.aTCMediator = aTCMediator;
+            aTCMediator.SetLandingStatus(true);
+        }
+        public void Land()
+        {
+            Console.WriteLine("Landing permission granted");
+            aTCMediator.SetLandingStatus(true);
+        }
+    }
+}
