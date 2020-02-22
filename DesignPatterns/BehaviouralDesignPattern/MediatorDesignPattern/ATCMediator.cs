56eab155b1b7eec2634f43d8687ff4fbb371b304
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 16:39:12 +0530

    [tejasri]Add . Implementation of ATCMediator class in Mediator Design Pattern

---
+++ DesignPatterns/BehaviouralDesignPattern/MediatorDesignPattern/ATCMediator.cs
@@ -0,0 +1,35 @@
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="ATCMediator.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.BehaviouralDesignPattern.MediatorDesignPattern
+{
+    public class ATCMediator : IATCMediator
+    {
+        private Flight flight;
+        private Runway runway;
+        public bool land;
+
+        public void RegisterRunway(Runway runway)
+        {
+            this.runway = runway;
+        }
+
+        public void RegisterFlight(Flight flight)
+        {
+            this.flight = flight;
+        }
+
+        public bool IsLandingOk()
+        {
+            return land;
+        }
+
+        public void SetLandingStatus(bool status)
+        {
+            land = status;
+        }
+    }
+}
