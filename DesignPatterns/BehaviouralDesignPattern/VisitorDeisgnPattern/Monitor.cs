7a69ca89cefa9212434f4a0d13e9876b3b29cd26
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 12:06:44 +0530

    [tejasri]Refactor . Modified Monitor class in Visitor Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/Monitor.cs
+++ DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/Monitor.cs
@@ -6,8 +6,16 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern
 {
+    /// <summary>
+    /// Monitor class implements IComputerPart interface and overrides Accept method
+    /// </summary>
+    /// <seealso cref="DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern.IComputerPart" />
     public class Monitor : IComputerPart
     {
+        /// <summary>
+        /// Accepts the specified computer part visitor.
+        /// </summary>
+        /// <param name="computerPartVisitor">The computer part visitor.</param>
         public void Accept(IComputerPartVisitor computerPartVisitor)
         {
             computerPartVisitor.Visit(this);
