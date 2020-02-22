23aacb7d254c3347d7dfec4e51c65301052c200c
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 11:55:29 +0530

    [tejasri]Refactor . Modified Computer class in Visitor Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/Computer.cs
+++ DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/Computer.cs
@@ -7,21 +7,37 @@
 namespace DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern
 {
     using System.Collections.Generic;
+
+    /// <summary>
+    /// Computer class implements IComputerPart interface 
+    /// </summary>
+    /// <seealso cref="DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern.IComputerPart" />
     public class Computer : IComputerPart
     {
+        /// <summary>
+        /// The parts
+        /// </summary>
+        private List<IComputerPart> parts;
 
-        List<IComputerPart> parts;
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Computer"/> class.
+        /// </summary>
         public Computer()
         {
-            this.parts = new List<IComputerPart>{ new Mouse(), new Keyboard(), new Monitor() };
+            this.parts = new List<IComputerPart> { new Mouse(), new Keyboard(), new Monitor() };
         }
+
+        /// <summary>
+        /// Accepts the specified computer part visitor.
+        /// </summary>
+        /// <param name="computerPartVisitor">The computer part visitor.</param>
         public void Accept(IComputerPartVisitor computerPartVisitor)
         {
-            foreach(var part in parts)
+            foreach (var part in this.parts)
             {
                 part.Accept(computerPartVisitor);
             }
+
             computerPartVisitor.Visit(this);
         }
     }
