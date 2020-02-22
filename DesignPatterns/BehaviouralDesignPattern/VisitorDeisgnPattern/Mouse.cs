1a139b33d944f277ad5c146a638a0e45abe3bdd8
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-22 12:08:13 +0530

    [tejasri]Refactor . Modified Mouse class in Visitor Design Pattern

--- DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/Mouse.cs
+++ DesignPatterns/BehaviouralDesignPattern/VisitorDeisgnPattern/Mouse.cs
@@ -6,8 +6,16 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern
 {
+    /// <summary>
+    /// Mouse class implements IComputerPart interface and overrides Accept method
+    /// </summary>
+    /// <seealso cref="DesignPatterns.BehaviouralDesignPattern.VisitorDeisgnPattern.IComputerPart" />
     public class Mouse : IComputerPart
     {
+        /// <summary>
+        /// Accepts the specified computer part visitor.
+        /// </summary>
+        /// <param name="computerPartVisitor">The computer part visitor.</param>
         public void Accept(IComputerPartVisitor computerPartVisitor)
         {
             computerPartVisitor.Visit(this);
