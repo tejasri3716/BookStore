6b83cbb75225e4077b0d33083c893b75658ab610
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 12:58:12 +0530

    [tejasri]Refactor . Modified DependencyTest class in Dependency Injection

--- DesignPatterns/DependencyInjection/DependencyInjectionTest.cs
+++ DesignPatterns/DependencyInjection/DependencyInjectionTest.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.DependencyInjection
+﻿namespace DesignPatterns.DependencyInjection
 {
+    /// <summary>
+    /// DependencyInjectionTest class creates objects of Service class and Customer class
+    /// </summary>
     public class DependencyInjectionTest
     {
+        /// <summary>
+        /// Tests this instance.
+        /// </summary>
         public static void Test()
         {
-            //creating object
+            // creating object 
             Service service = new Service();
-            //passing dependency
+
+            // passing dependency
             Client c1 = new Client(service);
             c1.ServeMethod();
            Customer customer = new Customer();
-            //passing dependency
+
+            // passing dependency
             c1 = new Client(customer);
             c1.ServeMethod();
         }
