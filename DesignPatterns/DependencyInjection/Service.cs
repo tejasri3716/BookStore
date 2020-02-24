c708d5beca6502b604c075b395df476225f35fa0
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 12:43:14 +0530

    [tejasri]Add . Implementation of Service class in Dependency Injection

---
+++ DesignPatterns/DependencyInjection/Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DependencyInjection
+{
+    public class Service : IService
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Service class is called");
+        }
+    }
+}
