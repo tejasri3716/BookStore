41102bbe205063933a004a6c111c0158f778ee20
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 13:02:41 +0530

    [tejasri]Refactor . Modified Client class in Dependency Injection

--- DesignPatterns/DependencyInjection/Client.cs
+++ DesignPatterns/DependencyInjection/Client.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.DependencyInjection
+﻿namespace DesignPatterns.DependencyInjection
 {
+    /// <summary>
+    /// Client class consists of a parameterized constructor which invokes IService interface
+    /// </summary>
     public class Client
     {
+        /// <summary>
+        /// The service
+        /// </summary>
         private IService service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Client"/> class.
+        /// </summary>
+        /// <param name="service">The service.</param>
         public Client(IService service)
         {
             this.service = service;
         }
+
+        /// <summary>
+        /// Serves the method.
+        /// </summary>
         public void ServeMethod()
         {
             this.service.Serve();
