dc6e7994ab4068c8d050849122814fdfb47ce883
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 12:53:35 +0530

    [tejasri]Refactor . Modified Service class in Dependency Injection

--- DesignPatterns/DependencyInjection/Service.cs
+++ DesignPatterns/DependencyInjection/Service.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.DependencyInjection
+﻿namespace DesignPatterns.DependencyInjection
 {
+    using System;
+
+    /// <summary>
+    /// Service class implements IService interface and overrides Serve method
+    /// </summary>
+    /// <seealso cref="DesignPatterns.DependencyInjection.IService" />
     public class Service : IService
     {
+        /// <summary>
+        /// Serves this instance.
+        /// </summary>
         public void Serve()
         {
             Console.WriteLine("Service class is called");
