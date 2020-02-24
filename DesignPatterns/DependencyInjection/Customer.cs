25e4a420330fd9c593d4a4e097dfb64ee918ab54
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 13:04:34 +0530

    [tejasri]Refactor . Modified Customer class in Dependency Injection

--- DesignPatterns/DependencyInjection/Customer.cs
+++ DesignPatterns/DependencyInjection/Customer.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.DependencyInjection
+﻿namespace DesignPatterns.DependencyInjection
 {
+    using System;
+
+    /// <summary>
+    /// Customer class implements IService interface
+    /// </summary>
+    /// <seealso cref="DesignPatterns.DependencyInjection.IService" />
     public class Customer : IService
     {
+        /// <summary>
+        /// Serves this instance.
+        /// </summary>
         public void Serve()
         {
             Console.WriteLine("Customer class is called");
