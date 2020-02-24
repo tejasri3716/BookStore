2ba6a76e5f9b8ac8b4b0be3529557c70e669fce7
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 12:44:46 +0530

    [tejasri]Add . Implementation of Client class in Dependency Injection

---
+++ DesignPatterns/DependencyInjection/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DependencyInjection
+{
+    public class Client
+    {
+        private IService service;
+        public Client(IService service)
+        {
+            this.service = service;
+        }
+        public void ServeMethod()
+        {
+            this.service.Serve();
+        }
+    }
+}
