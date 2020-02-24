1b2627dd038d6d4006a7f97f245d5f16d4f50697
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 12:49:43 +0530

    [tejasri]Refactor .Modified Program class in Design Patterns

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -22,7 +22,7 @@ namespace DesignPatterns
             Console.WriteLine("Singleton Design Pattern\n1.LazyIntializationSingleton\n" +
                 "2.ThreadSafeSingleton\n3.EagerIntialization\n4.FactoryDesignPattern\n5.PrototypeDesignPattern" +
                 "\n6.Adapter Design Pattern\n7.FacadeDesignPattern\n8.ProxyDesignPattern\n9.ObserverDesignPattern" +
-                "\n10.VisitorDesignPattern\n11.MediatorDesignPattern\n12.Reflection");
+                "\n10.VisitorDesignPattern\n11.MediatorDesignPattern\n12.Reflection\n13.DependencyInjection");
             Console.WriteLine("enter an option to get executed");
             int option = Utility.ReadInt();
             switch (option)
@@ -62,6 +62,9 @@ namespace DesignPatterns
                     break;
                 case 12:
                     Reflection.ReflectionTest.Test();
+                    break;
+                case 13:
+                    DependencyInjection.DependencyInjectionTest.Test();
                     break;
                 default:
                     Console.WriteLine("enter correct option");
