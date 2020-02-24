6b8bd0b4a37e16612b189a174b2f8701523b07b2
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 13:09:30 +0530

    [tejasri]Refactor . Modified Service class in Dependency Injection

--- DesignPatterns/DependencyInjection/Service.cs
+++ DesignPatterns/DependencyInjection/Service.cs
@@ -1,4 +1,10 @@
-﻿namespace DesignPatterns.DependencyInjection
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="Service.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.DependencyInjection
 {
     using System;
 
