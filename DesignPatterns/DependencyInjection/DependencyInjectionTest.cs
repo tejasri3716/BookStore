381f8ce6afd32a5b7b3a22fc57f98f8fb50f9096
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 12:45:21 +0530

    [tejasri]Add . Implementation of DependencyInjectionTest class in Dependency Injection

---
+++ DesignPatterns/DependencyInjection/DependencyInjectionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DependencyInjection
+{
+    public class DependencyInjectionTest
+    {
+        public static void Test()
+        {
+            //creating object
+            Service service = new Service();
+            //passing dependency
+            Client c1 = new Client(service);
+            c1.ServeMethod();
+           Customer customer = new Customer();
+            //passing dependency
+            c1 = new Client(customer);
+            c1.ServeMethod();
+        }
+    }
+}
