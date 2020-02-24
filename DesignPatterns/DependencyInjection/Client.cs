f705333b6fb8b256a6787da13a7c872dca568934
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 13:05:59 +0530

    [tejasri]Refactor . Modified Client class in Dependency Injection

--- DesignPatterns/DependencyInjection/Client.cs
+++ DesignPatterns/DependencyInjection/Client.cs
@@ -1,4 +1,10 @@
-﻿namespace DesignPatterns.DependencyInjection
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="Client.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.DependencyInjection
 {
     /// <summary>
     /// Client class consists of a parameterized constructor which invokes IService interface
