b8baf27618932de611ebefcf8489a140b52d3616
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-24 12:44:15 +0530

    [tejasri]Add . Implementation of Customer class in Dependency Injection

---
+++ DesignPatterns/DependencyInjection/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DependencyInjection
+{
+    public class Customer : IService
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Customer class is called");
+        }
+    }
+}
