f71dcc8005da88b7725eaead04936761580a572e
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-25 12:58:21 +0530

    [tejasri]Refactor . Modified PrototypeTest class in Prototype Design Pattern

--- DesignPatterns/CreationalDesignpatterns/PrototypeDesignPattern/PrototypeTest.cs
+++ DesignPatterns/CreationalDesignpatterns/PrototypeDesignPattern/PrototypeTest.cs
@@ -12,41 +12,41 @@ namespace DesignPatterns.PrototypeDesignPattern
     /// </summary>
     class PrototypeTest
     {
-            /// <summary>
-            ///Test to execute all the operations
-            /// </summary>
-            public static void Test()
-            {
-                //// new Employee object creation 
-                Employee emp = new Employee();
+        /// <summary>
+        ///Test to execute all the operations
+        /// </summary>
+        public static void Test()
+        {
+            //// new Employee object creation 
+            Employee emp = new Employee();
 
-                ////input the name
-                Console.Write("enter employee name : ");
-                 emp.SetName(Utility.ReadString());
-                //// input the eid
-                Console.Write("enter employee id : ");
-                emp.SetEid(Utility.ReadInt());
-                ////input the age
-                Console.Write("enter employee age : ");
-                emp.SetAge(Utility.ReadInt());
+            ////input the name
+            Console.Write("enter employee name : ");
+            emp.SetName(Utility.ReadString());
+            //// input the eid
+            Console.Write("enter employee id : ");
+            emp.SetEid(Utility.ReadInt());
+            ////input the age
+            Console.Write("enter employee age : ");
+            emp.SetAge(Utility.ReadInt());
 
             //// print the details
             Console.WriteLine(emp.GetDetails());
 
-                //// clone the Employee object
-                Employee empobj = emp.Clone();
+            //// clone the Employee object
+            Employee empobj = emp.Clone();
 
-                //// imput employee name
-                Console.Write("enter employee name : ");
-                emp.SetName(Utility.ReadString());
+            //// imput employee name
+            Console.Write("enter employee name : ");
+            empobj.SetName(Utility.ReadString());
 
-                //// input employee id
-                Console.Write("enter employee id");
-                emp.SetEid(Utility.ReadInt());
+            //// input employee id
+            Console.Write("enter employee id");
+            empobj.SetEid(Utility.ReadInt());
 
-                //// print the details
-                Console.WriteLine(emp.GetDetails());
-                Console.WriteLine(empobj.GetDetails());
-            }
+            //// print the details
+            Console.WriteLine(emp.GetDetails());
+            Console.WriteLine(empobj.GetDetails());
+        }
     }
 }
