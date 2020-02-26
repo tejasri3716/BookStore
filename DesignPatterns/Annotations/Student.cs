6512bfb386582512913def3e5a35b47d4e12baa8
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-26 13:11:35 +0530

    [tejasri]Refactor . Modified Student class in Annotations

--- DesignPatterns/Annotations/Student.cs
+++ DesignPatterns/Annotations/Student.cs
@@ -1,39 +1,73 @@
-﻿namespace DesignPatterns.Annotations
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="Student.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.Annotations
 {
+    /// <summary>
+    /// Student class used to write custom attributes
+    /// </summary>
     public class Student
     {
-		// Private fields of class Student 
-		private int rollNo;
-		private string studentName;
-		private double marks;
+        /// <summary>
+        /// The roll number
+        /// </summary>
+        private int rollNo;
 
-		// The attribute CustomAttribute is applied 
-		// to methods of class Student 
-		// Providing details of their utility 
-		[CustomAttribute("Modifier", "Assigns the Student Details")]
-		public void SetDetails(int r, string sn, double m)
-		{
-			rollNo = r;
-			studentName = sn;
-			marks = m;
-		}
+        /// <summary>
+        /// The student name
+        /// </summary>
+        private string studentName;
 
-		[CustomAttribute("Accessor", "Returns Value of rollNo")]
-		public int GetRollNo()
-		{
-			return rollNo;
-		}
+        /// <summary>
+        /// The marks
+        /// </summary>
+        private double marks;
 
-		[CustomAttribute("Accessor", "Returns Value of studentName")]
-		public string GetStudentName()
-		{
-			return studentName;
-		}
+        /// <summary>
+        /// Sets the details.
+        /// </summary>
+        /// <param name="rno">The r.</param>
+        /// <param name="sname">The sn.</param>
+        /// <param name="marks">The m.</param>
+        [CustomAttribute("Modifier", "Assigns the Student Details")]
+        public void SetDetails(int rno, string sname, double marks)
+        {
+            this.rollNo = rno;
+            this.studentName = sname;
+            this.marks = marks;
+        }
 
-		[CustomAttribute("Accessor", "Returns Value of marks")]
-		public double GetMarks()
-		{
-			return marks;
-		}
-	}
+        /// <summary>
+        /// Gets the roll no.
+        /// </summary>
+        /// <returns>returns roll number</returns>
+        [CustomAttribute("Accessor", "Returns Value of rollNo")]
+        public int GetRollNo()
+        {
+            return this.rollNo;
+        }
+
+        /// <summary>
+        /// Gets the name of the student.
+        /// </summary>
+        /// <returns>returns student name</returns>
+        [CustomAttribute("Accessor", "Returns Value of studentName")]
+        public string GetStudentName()
+        {
+            return this.studentName;
+        }
+
+        /// <summary>
+        /// Gets the marks.
+        /// </summary>
+        /// <returns>return marks</returns>
+        [CustomAttribute("Accessor", "Returns Value of marks")]
+        public double GetMarks()
+        {
+            return this.marks;
+        }
+    }
 }
