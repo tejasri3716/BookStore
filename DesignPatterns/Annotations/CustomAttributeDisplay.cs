849c9c6a7e70863a52cc6dd186f5acf705d3baad
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-02-26 13:13:45 +0530

    [tejasri]Refactor . Modified CustomAttributeDisplay class in Annotations

--- DesignPatterns/Annotations/CustomAttributeDisplay.cs
+++ DesignPatterns/Annotations/CustomAttributeDisplay.cs
@@ -1,19 +1,31 @@
-﻿namespace DesignPatterns.Annotations
+﻿//--------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomAttributeDisplay.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace DesignPatterns.Annotations
 {
-	using System;
+    using System;
 
-	public class CustomAttributeDisplay
-	{
-		public static void DisplayAttributes()
-		{
-			Student s = new Student();
-			s.SetDetails(89, "Tejasri", 92.5);
-			CustomAttribute.AttributeDisplay(typeof(Student));
-			Console.WriteLine("");
-			Console.WriteLine("Student Details");
-			Console.WriteLine("Roll Number : " + s.GetRollNo());
-			Console.WriteLine("Name : " + s.GetStudentName());
-			Console.WriteLine("Marks : " + s.GetMarks());
-		}
-	}
+    /// <summary>
+    /// CustomAttributeDisplay class displays the attributes
+    /// </summary>
+    public class CustomAttributeDisplay
+    {
+        /// <summary>
+        /// Displays the attributes.
+        /// </summary>
+        public static void DisplayAttributes()
+        {
+            Student s = new Student();
+            s.SetDetails(89, "Tejasri", 92.5);
+            CustomAttribute.AttributeDisplay(typeof(Student));
+            Console.Write("\n");
+            Console.WriteLine("Student Details");
+            Console.WriteLine("Roll Number : " + s.GetRollNo());
+            Console.WriteLine("Name : " + s.GetStudentName());
+            Console.WriteLine("Marks : " + s.GetMarks());
+        }
+    }
 }
