c6cf2296546a3304a1a97f38b405c5a0d71dcf60
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-02 19:11:14 +0530

    [tejasri]Add . Implementation of Manager in Employee Management System

---
+++ EmployeeManagementSystem/Manager/EmployeeManager.cs
@@ -0,0 +1,74 @@
+﻿
+
+namespace EmployeeManagementSystem.Manager
+{
+    using EmployeeManagementSystem.Model;
+    using EmployeeManagementSystem.Repository;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// EmployeeManager class implements IEmployeeManager
+    /// </summary>
+    /// <seealso cref="EmployeeManagement.EmployeeManager.IEmployeeManager" />
+    public class EmployeeManager : IEmployeeManager
+    {
+        /// <summary>
+        /// The repository
+        /// </summary>
+        private IEmployee repository = new EmployeeOperations();
+
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns>
+        /// Returns a list of all employees.
+        /// </returns>
+        public List<Employee> GetEmployees()
+        {
+            return this.repository.GetEmployees();
+        }
+
+        /// <summary>
+        /// Adds the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        public void AddEmployee(Employee employee)
+        {
+            this.repository.AddEmployee(employee);
+        }
+
+        /// <summary>
+        /// Updates the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        public void UpdateEmployee(Employee employee)
+        {
+            this.repository.UpdateEmployee(employee);
+        }
+
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public void DeleteEmployee(string email)
+        {
+            this.repository.DeleteEmployee(email);
+        }
+
+        /// <summary>
+        /// Logins the specified email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>
+        /// Returns true if email and password exist else false.
+        /// </returns>
+        public bool LoginEmployee(string email, string password)
+        {
+            return this.repository.LoginEmployee(email, password);
+        }
+    }
+}
---
+++ EmployeeManagementSystem/Manager/IEmployeeManager.cs
@@ -0,0 +1,49 @@
+﻿namespace EmployeeManagementSystem.Manager
+{
+    using EmployeeManagementSystem.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    /// <summary>
+    /// Interface class IEmployeeManager
+    /// </summary>
+    /// <summary>
+    public interface IEmployeeManager
+    {
+        /// Gets the employees.
+        /// </summary>
+        /// <returns>
+        /// Gets all Emloyees
+        /// </returns>
+        List<Employee> GetEmployees();
+
+        /// <summary>
+        /// Adds the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        void AddEmployee(Employee employee);
+
+        /// <summary>
+        /// Updates the specified employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        void UpdateEmployee(Employee employee);
+
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        void DeleteEmployee(string Email);
+
+        /// <summary>
+        /// Logins the employee.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>
+        /// Returns true if email and password exist else false.
+        /// </returns>
+        bool LoginEmployee(string email, string password);
+    }
+}
