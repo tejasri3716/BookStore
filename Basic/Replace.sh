39d2952c75255e73ea1b3b2aa3e00cf549b048cf
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-11 11:15:58 +0530

    [tejasri]Refactor . Modified Shell Script Programs

--- Basic/Replace.sh
+++
@@ -1,8 +0,0 @@
-echo "hello <<username>>,How are you?"
-read username
-if [ ${#username} -ge '3' ]
-then
-echo "Hello $username , How are you?"
-else
-echo "invalid input"
-fi
--- Logical/StopWatch.sh
+++
@@ -1,8 +0,0 @@
-read -p "Enter any key to start time: " begin
-start="$(date +"%s%N")"
-echo $start
-read -p "Enter any key to stop time:  "  end
-stop="$(date +"%s%N")"
-echo $stop
- ((result=$start-$stop))
-echo $result
