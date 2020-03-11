dfc48ffdb0d18015030afed21ba5ce54731dbcb6
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-11 11:41:57 +0530

    [tejasri]Refactor . Modified Repository in EmployeeManagementSystem

--- EmployeeManagement/EmployeeManagementSystem/Repository/EmployeeOperations.cs
+++ EmployeeManagement/EmployeeManagementSystem/Repository/EmployeeOperations.cs
@@ -1,16 +1,30 @@
-﻿using EmployeeManagementSystem.Model;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=DeckOfCardsUsingQ.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementSystem.Repository
 {
+    using EmployeeManagementSystem.Model;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    /// <summary>
+    /// Employee Operations class implements IEmployee interface
+    /// </summary>
     public class EmployeeOperations : IEmployee
     {
         string conn = (@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True");
+
+        /// <summary>
+        /// Add Employee method retreivesw the data from database
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns>
+        /// returns added if added successfully into database
+        /// </returns>
         public string AddEmployee(Employee employee)
         {
             using (SqlConnection con = new SqlConnection(conn))
@@ -28,6 +42,14 @@ namespace EmployeeManagementSystem.Repository
                 return "Added";
             }
         }
+
+        /// <summary>
+        /// Delete Employee method deletes a particular employee details
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <returns>
+        /// return deleted on successfull deletion
+        /// </returns>
         public string DeleteEmployee(string Email)
         {
             using (SqlConnection con = new SqlConnection(conn))
@@ -42,6 +64,13 @@ namespace EmployeeManagementSystem.Repository
                 return "Deleted";
             }
         }
+
+        /// <summary>
+        /// Get Employees method is used to get the list of all employees in the database
+        /// </summary>
+        /// <returns> 
+        /// returns the list of employees
+        /// </returns>
         public List<Employee> GetEmployees()
         {
             List<Employee> list = new List<Employee>();
@@ -64,6 +93,15 @@ namespace EmployeeManagementSystem.Repository
             }
             return list;
         }
+
+        /// <summary>
+        /// Login Employee method is used to check the credentials of the user
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns>
+        /// returns true status on successfull login
+        /// </returns>
         public bool LoginEmployee(string email, string password)
         {
             using (SqlConnection con = new SqlConnection(this.conn))
@@ -77,9 +115,9 @@ namespace EmployeeManagementSystem.Repository
                 SqlDataAdapter adapter = new SqlDataAdapter(cmd);
                 adapter.Fill(dataSet);
                 con.Close();
-                bool loginSuccessful = ((dataSet.Tables.Count > 0) && (dataSet.Tables[0].Rows.Count > 0));
+                bool login = ((dataSet.Tables.Count > 0) && (dataSet.Tables[0].Rows.Count > 0));
 
-                if (loginSuccessful && email != "" && password != "")
+                if (login && email != "" && password != "")
                 {
                     return true;
                 }
@@ -89,6 +127,14 @@ namespace EmployeeManagementSystem.Repository
                 }
             }
         }
+
+        /// <summary>
+        /// Update Employee method is used to update the particular user details
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns>
+        /// returns updated on successfull updation in the database
+        /// </returns>
         public string UpdateEmployee(Employee employee)
         {
             using (SqlConnection con = new SqlConnection(conn))
--- EmployeeManagement/EmployeeManagementSystem/Repository/IEmployee.cs
+++ EmployeeManagement/EmployeeManagementSystem/Repository/IEmployee.cs
@@ -1,17 +1,52 @@
-﻿using EmployeeManagementSystem.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=DeckOfCardsUsingQ.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementSystem.Repository
 {
+    using EmployeeManagementSystem.Model;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// IEmployee interface consists of abstract methods
+    /// </summary>
     public interface IEmployee
     {
+        /// <summary>
+        /// Gets the employees.
+        /// </summary>
+        /// <returns></returns>
         List<Employee> GetEmployees();
+
+        /// <summary>
+        /// Adds the employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <returns></returns>
         string AddEmployee(Employee employee);
+
+        /// <summary>
+        /// Deletes the employee.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns></returns>
         string DeleteEmployee(string email);
+
+        /// <summary>
+        /// Updates the employee.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <returns></returns>
         string UpdateEmployee(Employee employee);
+
+        /// <summary>
+        /// Logins the employee.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
         bool LoginEmployee(string email, string password);
     }
 }
