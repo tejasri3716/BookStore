913a7f8367d969ba1a4aa5010ac61d443cb32f49
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-11 11:42:30 +0530

    [tejasri]Refactor . Modified Program class in EmployeeManagementSystem

--- EmployeeManagement/EmployeeManagementSystem/Program.cs
+++ EmployeeManagement/EmployeeManagementSystem/Program.cs
@@ -1,22 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=DeckOfCardsUsingQ.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementSystem
 {
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    /// <summary>
+    /// Program class is used to call start up class 
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates the web host builder.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
