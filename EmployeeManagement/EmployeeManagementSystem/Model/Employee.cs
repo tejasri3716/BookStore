a17bfb504fa4926a6d6fc3fc76d3148ca4fa95e9
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-11 11:41:02 +0530

    [tejasri]Refactor . Modified Model class in EmployeeManagementSystem

--- EmployeeManagement/EmployeeManagementSystem/Model/Employee.cs
+++ EmployeeManagement/EmployeeManagementSystem/Model/Employee.cs
@@ -1,23 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=DeckOfCardsUsingQ.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementSystem.Model
 {
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Employee class is the model class of Employee Management System
+    /// </summary>
     public class Employee
     {
+        /// <summary>
+        /// The firstname
+        /// </summary>
         private string firstname;
+
+        /// <summary>
+        /// The lastname
+        /// </summary>
         private string lastname;
+
+        /// <summary>
+        /// The email
+        /// </summary>
         private string email;
+
+        /// <summary>
+        /// The password
+        /// </summary>
         private string password;
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
         [Required]
         public string FirstName { get => this.firstname; set => this.firstname = value; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
         [Required]
         public string LastName { get => this.lastname; set => this.lastname = value; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         [Required]
         public string Email { get => this.email; set => this.email = value; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
         [Required]
         public string Password { get => this.password; set => this.password = value; }
     }
