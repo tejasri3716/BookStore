ba88bac81b450eb5593c8e920b86b525bff5b0d6
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-11 11:45:19 +0530

    [tejasri]Refactor . Modified EmployeeManagementTest class in EmployeeManagementTestCases

--- EmployeeManagement/EmployeeManagementTestCases/EmployeeManagementTest.cs
+++ EmployeeManagement/EmployeeManagementTestCases/EmployeeManagementTest.cs
@@ -1,28 +1,42 @@
-using EmployeeManagementSystem.Manager;
-using EmployeeManagementSystem.Controller;
-using NUnit.Framework;
-using Microsoft.AspNetCore.Mvc;
-using EmployeeManagementSystem.Model;
-
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=DeckOfCardsUsingQ.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementTestCases
 {
+    using EmployeeManagementSystem.Manager;
+    using NUnit.Framework;
+    using EmployeeManagementSystem.Model;
+
+    /// <summary>
+    /// Tests class is used to Test the methods of Employee Management System
+    /// </summary>
     [TestFixture]
-    public class Tests
+    public class EmployeeManagementTest
     {
+        /// <summary>
+        /// Tests add method of EmployeeManager class
+        /// </summary>
         [Test]
         public void TestAdd()
         {
             EmployeeManager manager = new EmployeeManager();
-            Employee employee = new Employee() { 
-                FirstName = "abc",
-                LastName = "cd",
-                Email = "abcd@gmail.com",
-                Password = "111123"
+            Employee employee = new Employee()
+            {
+                FirstName = "absdfrgdfc",
+                LastName = "cddf",
+                Email = "bfaed@gmail.com",
+                Password = "11114323"
             };
             var result = manager.AddEmployee(employee);
             Assert.AreEqual("Added", result);
         }
 
+        /// <summary>
+        /// Tests delete method of EmployeeManager class
+        /// </summary>
         [Test]
         public void TestDelete()
         {
@@ -32,6 +46,9 @@ namespace EmployeeManagementTestCases
             Assert.AreEqual("Deleted", result);
         }
 
+        /// <summary>
+        /// Tests update method of EmployeeManager class
+        /// </summary>
         [Test]
         public void TestUpdate()
         {
@@ -47,6 +64,9 @@ namespace EmployeeManagementTestCases
             Assert.AreEqual("Updated", result);
         }
 
+        /// <summary>
+        /// Tests login method of EmployeeManager class
+        /// </summary>
         [Test]
         public void TestLogin()
         {
@@ -55,7 +75,6 @@ namespace EmployeeManagementTestCases
             string password = "indu@3716";
             var result = manager.LoginEmployee(email, password);
             Assert.AreEqual("True", result.ToString());
-
         }
     }
 }
