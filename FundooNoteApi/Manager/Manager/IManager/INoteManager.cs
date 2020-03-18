cd127e0264956867796857d7b16b45b2e7ea1084
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-18 14:51:54 +0530

    [tejasri]Refactor . Modified Manager in FundooNoteApi

---
+++ FundooNoteApi/Manager/Manager/IManager/INoteManager.cs
@@ -0,0 +1,14 @@
+﻿namespace Manager.IManager
+{
+    using Model.NoteModel;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    public interface INoteManager
+    {
+        Task<string> AddNotes(NoteModel note);
+        Task<string> DeleteNote(int id);
+        List<NoteModel> GetNote(int id);
+        Task<string> UpdateNote(NoteModel note);
+        List<NoteModel> GetAllNotes();
+    }
+}
