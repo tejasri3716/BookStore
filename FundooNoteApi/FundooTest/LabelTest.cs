99f37cd3943cf39e635347c50caff2bec954ffdd
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-19 22:59:21 +0530

    [tejasri]Refactor . Added Label Test in FundooNoteApi

---
+++ FundooNoteApi/FundooTest/LabelTest.cs
@@ -0,0 +1,35 @@
+﻿using Manager.Manager;
+using Model.LabelModels;
+using NUnit.Framework;
+using Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooTest
+{
+    [TestFixture]
+    public class LabelTest
+    {
+        [Test]
+        public void AddLabelTest()
+        {
+            LabelRepository repository = new LabelRepository();
+            LabelModel label = new LabelModel
+            {
+                LabelId=1,
+                LabelName="Sample",
+                NoteId=1
+            };
+            Assert.IsNotNull(repository.AddLabel(label));
+        }
+
+        [Test]
+        public void DeleteLabel()
+        {
+            LabelManager label = new LabelManager();
+            var result = label.DeleteLabel(1);
+            Assert.NotNull(result);
+        }
+    }
+}
