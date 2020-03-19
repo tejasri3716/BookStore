2c73401998bf4350d4e8d486c3aed68508de979c
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-19 22:48:25 +0530

    [tejasri]Refactor . Modified Label Model in FundooNoteApi

---
+++ FundooNoteApi/Model/LabelModels/LabelModel.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=LabelModel.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Model.LabelModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// LabelModel class is used as model class
+    /// </summary>
+    public class LabelModel
+    {
+        /// <summary>
+        /// Label Id
+        /// </summary>
+        private int labelId;
+
+        /// <summary>
+        /// Note Id
+        /// </summary>
+        private int noteId;
+
+        /// <summary>
+        /// Label Name
+        /// </summary>
+        private string labelName;
+        
+        /// <summary>
+        /// Label Id property
+        /// </summary>
+        [Key]
+        public int LabelId { get => this.labelId; set => this.labelId = value; }
+
+        /// <summary>
+        /// Note Id property
+        /// </summary>
+        public int NoteId { get => this.noteId; set => this.noteId = value; }
+
+        /// <summary>
+        /// Label Name property
+        /// </summary>
+        public string LabelName { get => this.labelName; set => this.labelName = value; }
+    }
+}
