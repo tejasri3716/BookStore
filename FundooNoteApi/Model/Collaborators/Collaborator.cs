bf4c56ed3eb1631a3a4ffb124788a233792256b1
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-19 22:50:07 +0530

    [tejasri]Refactor . Modified Collaborators

--- FundooNoteApi/Model/Collaborators/Collaborator.cs
+++ FundooNoteApi/Model/Collaborators/Collaborator.cs
@@ -4,14 +4,44 @@
 // </copyright>
 // <creator name="tejasri"/>
 // --------------------------------------------------------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Model.Collaborators
 {
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Collaborator class is used as model class for Collaborator
+    /// </summary>
     public class Collaborator
     {
+        /// <summary>
+        /// Note ID 
+        /// </summary>
+        private int noteId;
 
+        /// <summary>
+        /// Email of the sender 
+        /// </summary>
+        private string senderEmail;
+
+        /// <summary>
+        /// Receiver Email
+        /// </summary>
+        private string receiverEmail;
+
+        /// <summary>
+        /// Note Id property
+        /// </summary>
+        public int NoteId { get => this.noteId; set => this.noteId = value; }
+
+        /// <summary>
+        /// Sender Email property
+        /// </summary>
+        [Key]
+        public string SenderEmail { get => this.senderEmail; set => this.senderEmail = value; }
+
+        /// <summary>
+        /// Receiver Email property
+        /// </summary>
+        public string ReceiverEmail { get => this.receiverEmail; set => this.receiverEmail = value; }
     }
 }
