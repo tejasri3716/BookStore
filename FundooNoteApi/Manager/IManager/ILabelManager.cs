7a3a01e74526120eac0d2b7207d058258c47a7e6
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-19 22:43:35 +0530

    [tejasri]Refactor . Modified IManager interface in FundooNoteApi

---
+++ FundooNoteApi/Manager/IManager/ILabelManager.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=ILabelManager.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Manager.IManager
+{
+    using Model.LabelModels;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Label Manager interface consists of abstract methods
+    /// </summary>
+    public interface ILabelManager
+    {
+        /// <summary>
+        /// Adds the specified label model.
+        /// </summary>
+        /// <param name="labelModel">The label model.</param>
+        /// <returns></returns>
+        string AddLabel(LabelModel labelModel);
+
+        /// <summary>
+        /// Updates the specified label model.
+        /// </summary>
+        /// <param name="labelModel">The label model.</param>
+        /// <returns></returns>
+        string UpdateLabel(int id, string name);
+
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        string DeleteLabel(int id);
+
+        /// <summary>
+        /// Gets all list.
+        /// </summary>
+        /// <returns></returns>
+        List<LabelModel> GetAllLabels();
+
+        /// <summary>
+        /// Gets the label.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        List<LabelModel> GetLabel(int id);
+    }
+}
