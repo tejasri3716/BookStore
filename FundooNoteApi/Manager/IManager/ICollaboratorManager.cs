012c2ed1b146e8c5ff195d252fcdd5cc4798eb96
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-26 12:56:15 +0530

    [tejasri]Refactor . Added ICollaboratorManager interface in Manager

---
+++ FundooNoteApi/Manager/IManager/ICollaboratorManager.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AccountController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Manager.IManager
+{
+    using Model.Collaborators;
+    using System.Threading.Tasks;
+    public interface ICollaboratorManager
+    {
+        /// <summary>
+        /// Adds the collaborator.
+        /// </summary>
+        /// <param name="collaborator">The collaborator.</param>
+        /// <returns></returns>
+        string AddCollaborator(Collaborator collaborator);
+
+        /// <summary>
+        /// Removes the collaborator.
+        /// </summary>
+        /// <param name="collaborator">The collaborator.</param>
+        /// <returns></returns>
+        string DeleteCollaborator(int id);
+    }
+}
