aa8541221dbac67b1026a5f0cd97270caaf744f2
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-26 13:11:47 +0530

    [tejasri]Refactor . Modified ICollaborator in IRepository

---
+++ FundooNoteApi/Repository/IRepository/ICollaborator.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AccountController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Repository.IRepository
+{
+    using Model.Collaborators;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// ICollaborator interface consists of abstract methods
+    /// </summary>
+    public interface ICollaborator
+    {
+        /// <summary>
+        /// Adds the collaborators to notes.
+        /// </summary>
+        /// <param name="collaborator">The collaborator .</param>
+        /// <returns></returns>
+        string AddCollaboratorToNotes(Collaborator collaborator);
+
+        /// <summary>
+        /// Deletes the collaborator.
+        /// </summary>
+        /// <param name="collaborator">The collaborator .</param>
+        /// <returns></returns>
+        string DeleteCollaborator(int id);
+    }
+}
