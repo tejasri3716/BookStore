5a7e863d0e3e3c0297fab5eac8a7979a9bad9947
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-26 13:35:19 +0530

    [tejasri]Refactor . Added Collaborator Repository in Repository

---
+++ FundooNoteApi/Repository/RepositoryClasses/CollaboratorRepository.cs
@@ -0,0 +1,96 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AccountController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Repository.RepositoryClasses
+{
+    using global::Repository.Context;
+    using global::Repository.IRepository;
+    using Model.Collaborators;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Collaboratory Repository class extends ICollaborator interface
+    /// </summary>
+    /// <seealso cref="Repository.IRepository.ICollaborator" />
+    public class CollaboratorRepository : ICollaborator
+    {
+        /// <summary>
+        /// UserContext private readonly variable
+        /// </summary>
+        private readonly UserContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollaboratorRepository"/> class.
+        /// </summary>
+        /// <param name="userContext">The user context.</param>
+        public CollaboratorRepository(UserContext userContext)
+        {
+            this.context = userContext;
+        }
+
+        /// <summary>
+        /// Adds the collaborators to notes.
+        /// </summary>
+        /// <param name="collaborator">The collaborator.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public string AddCollaboratorToNotes(Collaborator collaborator)
+        {
+            try
+            {
+                bool result = this.context.Notes.Any(option => option.Email == collaborator.SenderEmail && option.Id == collaborator.NoteId);
+                if (result)
+                {
+                    var user = this.context.Accountregister.Where(usr => usr.Email == collaborator.ReceiverEmail).SingleOrDefault();
+                    {
+                        var addCollaborator = new Collaborator()
+                        {
+                            NoteId = collaborator.NoteId,
+                            SenderEmail = collaborator.SenderEmail,
+                            ReceiverEmail = collaborator.ReceiverEmail
+                        };
+                        context.Collaborators.Add(collaborator);
+                    }
+                }
+                Task.Run(() => context.SaveChanges());
+                return "Added Successfully";
+                
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the collaborator.
+        /// </summary>
+        /// <param name="collaborator">The collaborator.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public string DeleteCollaborator(int id)
+        {
+            try
+            {
+                var result = this.context.Collaborators.Where(op => op.Id == id).SingleOrDefault();
+                if (result != null)
+                {
+                    this.context.Collaborators.Remove(result);
+                }
+                 Task.Run(() => context.SaveChanges());
+                return "Deleted Successfully";
+            }   
+            catch(Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+    }
+}
