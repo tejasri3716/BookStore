099bd6c19f344d7941403a3af95e06b68c57a8c2
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-26 13:02:56 +0530

    [tejasri]Refactor . Added CollaboratorManager in Manager

---
+++ FundooNoteApi/Manager/Manager/CollaboratorManager.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AccountController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="tejasri"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Manager.Manager
+{
+    using global::Manager.IManager;
+    using Model.Collaborators;
+    using Repository.IRepository;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CollaboratorManager : ICollaboratorManager
+    {
+        private readonly ICollaborator collaborator;
+
+        public CollaboratorManager(ICollaborator collaborator)
+        {
+            this.collaborator = collaborator;
+        }
+
+        /// <summary>
+        /// Adds the collaborator asynchronous.
+        /// </summary>
+        /// <param name="collaborator">The collaborator.</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public string AddCollaborator(Collaborator collaborator)
+        {
+            try
+            {
+                 var result=this.collaborator.AddCollaboratorToNotes(collaborator);
+                return result;
+            }
+            catch(Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+
+        public string DeleteCollaborator(int id)
+        {
+            try
+            {
+                 this.collaborator.DeleteCollaborator(id);
+                return "Deleted Successfully";
+            }
+            catch(Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+    }
+}
