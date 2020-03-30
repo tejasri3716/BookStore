57128354816f25fd2c80062f5a3802bc13c8ce66
tejasri3716 <saitejasri.kantheti@gmail.com>
2020-03-30 23:20:03 +0530

    [tejasri]Refactor . Modified INote Repository in Fundoo Note Api

--- FundooNoteApi/Repository/IRepository/INoteRepository.cs
+++ FundooNoteApi/Repository/IRepository/INoteRepository.cs
@@ -6,6 +6,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace Repository.IRepository
 {
+    using Microsoft.AspNetCore.Http;
     using Model.NoteModel;
     using System.Collections.Generic;
     using System.Threading.Tasks;
@@ -16,5 +17,18 @@ namespace Repository.IRepository
         Task DeleteNote(int id);
         List<NoteModel> GetAllNotes();
         List<NoteModel> GetNote(int id);
+        Task Trash(int id);
+        Task EmptyTrash();
+        List<NoteModel> TrashList();
+         Task Restore(int id);
+        Task RestoreAll();
+        Task IsArchive(int id);
+        Task UnArchive(int id);
+        List<NoteModel> ArchiveList();
+        Task Ispin(int id);
+        Task Unpin(int id);
+        int Reminder(int id, string reminder);
+        Task ChangeColor(int id, string color);
+        string UploadImage(IFormFile image, int id);
     }
 }
